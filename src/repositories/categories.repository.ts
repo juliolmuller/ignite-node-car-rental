b4caf6fe835fcd66ec2c0ef333e13de7bea78451
Julio Muller <juliodolago@gmail.com>
2022-08-08 23:01:56 -0300

    feat: define route to list all saved categories

--- src/repositories/categories.repository.ts
+++ src/repositories/categories.repository.ts
@@ -12,6 +12,10 @@ export class CategoriesRepository {
     this.categories = [];
   }
 
+  list(): Category[] {
+    return this.categories;
+  }
+
   create({ name, description }: ICreateCategoryDTO): Category {
     const category = new Category();
 
--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -4,6 +4,12 @@ import categoriesRepository from '../repositories/categories.repository';
 
 export const categoriesRoutes = Router();
 
+categoriesRoutes.get('/categories', (request, response) => {
+  const categories = categoriesRepository.list();
+
+  return response.status(200).json(categories);
+});
+
 categoriesRoutes.post('/categories', (request, response) => {
   const { name, description } = request.body;
   const category = categoriesRepository.create({ name, description });
