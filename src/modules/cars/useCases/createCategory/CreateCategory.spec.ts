9ba0cb97f8166afbef0e4e97e8f027e56e064767
Julio Muller <juliodolago@gmail.com>
2023-01-24 22:37:09 -0300

    chore: configure Jest

---
+++ src/modules/cars/useCases/createCategory/CreateCategory.spec.ts
@@ -0,0 +1,7 @@
+import { CreateCategoryService } from './CreateCategory.service';
+
+describe('CreateCategoryService', () => {
+  it('results 7 when summing 3 + 4', () => {
+    expect(3 + 4).toEqual(7);
+  });
+});
